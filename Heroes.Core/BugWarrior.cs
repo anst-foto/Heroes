e1cf9606f6bf56d88cea09cfc1b5556586a1670f
Starinin Andrey <starininandrey@gmail.com>
2023-08-26 17:29:39 +0300

    Add BugWarrior & DragonflyWarrior

---
+++ Heroes.Core/BugWarrior.cs
@@ -0,0 +1,16 @@
+﻿namespace Heroes.Core;
+
+public class BugWarrior : Bug, IWarrior
+{
+    private const int DAMAGE_RATE = 50;
+
+    public int DamageRate { get; init; }
+
+    public BugWarrior(string name) : base(name)
+    {
+        DamageRate = DAMAGE_RATE;
+    }
+
+
+    public void Attack(Hero hero) => hero.Health -= DamageRate;
+}
---
+++ Heroes.Core/DragonflyWarrior.cs
@@ -0,0 +1,11 @@
+﻿namespace Heroes.Core;
+
+public class DragonflyWarrior : Dragonfly, IWarrior
+{
+    private const int DAMAGE_RATE = 75;
+    public int DamageRate { get; init; }
+
+    public DragonflyWarrior(string name) : base(name) => DamageRate = DAMAGE_RATE;
+
+    public void Attack(Hero hero) => hero.Health -= DamageRate;
+}

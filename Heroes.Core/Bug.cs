cb76d2847f81b8e5ed09c173ffa6b089f54ebd39
Starinin Andrey <starininandrey@gmail.com>
2023-08-26 17:04:17 +0300

    Add Bug.cs & Dragonfly.cs

---
+++ Heroes.Core/Bug.cs
@@ -0,0 +1,10 @@
+﻿namespace Heroes.Core;
+
+public abstract class Bug : Hero
+{
+    private const int HEALTH = 200;
+
+    protected Bug(string name) : base(HEALTH, name)
+    {
+    }
+}
---
+++ Heroes.Core/Dragonfly.cs
@@ -0,0 +1,10 @@
+﻿namespace Heroes.Core;
+
+public abstract class Dragonfly : Hero
+{
+    private const int HEALTH = 100;
+
+    protected Dragonfly(string name) : base(HEALTH, name)
+    {
+    }
+}

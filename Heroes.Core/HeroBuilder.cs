127d1d769df834326da5e1a8463a95f362814708
Starinin Andrey <starininandrey@gmail.com>
2023-08-26 17:55:09 +0300

    Add HeroBuilder

---
+++ Heroes.Core/HeroBuilder.cs
@@ -0,0 +1,51 @@
+﻿namespace Heroes.Core;
+
+using Bugs;
+using Dragonflies;
+
+public enum HeroType { Bug, Dragonfly }
+
+public enum SpecType { Medic, Warrior }
+
+public class HeroBuilder
+{
+    private string name;
+    private HeroType heroType;
+    private SpecType specType;
+
+    public HeroBuilder AddName(string name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    public HeroBuilder SetHeroType(HeroType type)
+    {
+        this.heroType = type;
+        return this;
+    }
+
+    public HeroBuilder SetSpecType(SpecType type)
+    {
+        this.specType = type;
+        return this;
+    }
+
+    public Hero? Builder() =>
+        this.heroType switch
+        {
+            HeroType.Bug => this.specType switch
+            {
+                SpecType.Medic => new BugMedic(this.name),
+                SpecType.Warrior => new BugWarrior(this.name),
+                _ => null
+            },
+            HeroType.Dragonfly => this.specType switch
+            {
+                SpecType.Medic => new DragonflyMedic(this.name),
+                SpecType.Warrior => new DragonflyWarrior(this.name),
+                _ => null
+            },
+            _ => null
+        };
+}

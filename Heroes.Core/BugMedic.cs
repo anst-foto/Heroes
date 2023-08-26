e33d4a0ba88fa4ac78eafd39b2d4090146e6cb19
Starinin Andrey <starininandrey@gmail.com>
2023-08-26 17:12:07 +0300

    Add BugMedic & DragonflyMedic

---
+++ Heroes.Core/BugMedic.cs
@@ -0,0 +1,12 @@
+﻿namespace Heroes.Core;
+
+public class BugMedic : Bug, IMedic
+{
+    private const int HEALTH_RATE = 50;
+
+    public BugMedic(string name) : base(name)
+    {
+    }
+
+    public void Heal(Hero hero) => hero.Health += HEALTH_RATE;
+}
---
+++ Heroes.Core/DragonflyMedic.cs
@@ -0,0 +1,12 @@
+﻿namespace Heroes.Core;
+
+public class DragonflyMedic : Dragonfly, IMedic
+{
+    private const int HEALTH_RATE = 75;
+
+    public DragonflyMedic(string name) : base(name)
+    {
+    }
+
+    public void Heal(Hero hero) => hero.Health += HEALTH_RATE;
+}

fef97c7cc608dde696f1b096eca7ceeb3ed39df0
Starinin Andrey <starininandrey@gmail.com>
2023-08-26 17:00:56 +0300

    Add Hero.cs

---
+++ Heroes.Core/Hero.cs
@@ -0,0 +1,25 @@
+﻿namespace Heroes.Core;
+
+public abstract class Hero
+{
+    public string Name { get; init; }
+
+    private int health;
+    public int Health
+    {
+        get => this.health;
+        set
+        {
+            if (value < 0)
+            {
+                this.health = 0;
+            }
+        }
+    }
+
+    protected Hero(int health, string name)
+    {
+        this.health = health;
+        this.Name = name;
+    }
+}
